8a2ba6cd744b9ebb3bea588e994f552c0df69c2c
Damtab83 <damtab@hotmail.com>
2024-12-10 15:12:42 +0100

    Add Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LegalsNoticeComponent } from './pages/legals-notice/legals-notice.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'art-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LegalsNoticeComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Carine_Creation_Calista';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LegalsNoticeComponent } from './pages/legals-notice/legals-notice.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Route par défaut
+  { path: 'home', component: HomeComponent },
+  { path: 'mentions-legales', component: LegalsNoticeComponent },
+  // { path: 'contact', component: ContactComponent },
+  // { path: 'tableaux', component: TableauxComponent },
+  // { path: 'found', component: PageNotFoundComponent },
+];
